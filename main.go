822575608faa76f7db7403e13bfa7cedc4680cad
Hải Nguyễn <nvhai2408@gmail.com>
2023-11-15 00:50:11 +0700

    connect to db

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+func ConnectToDB(dns string) *sqlx.DB {
+	db, err := sqlx.Connect("mysql", dns)
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	// Check connection to DB by Ping
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Failed to ping the database:", err)
+	}
+	fmt.Println("Connected to the database!")
+
+	return db
+}
+
+func runServer() {
+	fmt.Print("cc")
+}
 
 func main() {
-	fmt.Print("Hello")
+	// Load the .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln(err)
+	}
+
+	dns := os.Getenv("DB_CONNECTION_STR")
+
+	db := ConnectToDB(dns)
+	fmt.Println(db)
 }
