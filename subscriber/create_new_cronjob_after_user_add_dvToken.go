6dffeaee773a097cc7067c1f52bd34ef91a02188
nvhai248 <nvhai2408@gmail.com>
2024-02-01 20:44:38 +0700

    fix pubsub when user add new dvToken

--- subscriber/create_new_cronjob_after_user_add_dvToken.go
+++ subscriber/create_new_cronjob_after_user_add_dvToken.go
@@ -35,70 +35,66 @@ func RunCreateNewCronJobAfterUserAddDeviceToken(appCtx component.AppContext) con
 				return appCtx.GetSendNotification().SendNotification(deviceToken, title, body)
 			}
 
-			go func() {
-				listHabit, err := habitStore.ListHabitByConditions(ctx, nil, nil, map[string]interface{}{"user_id": userData.GetUserId()})
+			listHabit, err := habitStore.ListHabitByConditions(ctx, nil, nil, map[string]interface{}{"user_id": userData.GetUserId()})
+
+			if err != nil {
+				return err
+			}
+
+			for _, h := range listHabit {
+				entryIds, err := appCtx.GetCronJob().CreateNewJobs(*common.NewNotificationBasedHabit(userData.GetUserId(),
+					h.Description,
+					h.Name,
+					h.StartDate,
+					h.EndDate,
+					h.Reminder,
+					*h.Days), sendNoticeFunc,
+					userData.GetLatestDvToken().DeviceToken,
+					fmt.Sprintf("Time to do %s", h.Name),
+					h.Description)
 
 				if err != nil {
-					return
+					return err
 				}
 
-				for _, h := range listHabit {
-					entryIds, err := appCtx.GetCronJob().CreateNewJobs(*common.NewNotificationBasedHabit(userData.GetUserId(),
-						h.Description,
-						h.Name,
-						h.StartDate,
-						h.EndDate,
-						h.Reminder,
-						*h.Days), sendNoticeFunc,
-						userData.GetLatestDvToken().DeviceToken,
-						fmt.Sprintf("Time to do %s", h.Name),
-						h.Description)
-
-					if err != nil {
-						return
-					}
-
-					for _, entryId := range entryIds {
-						store.CreateNewCronNoticeHabit(ctx, &cronnoticehabitmodel.CronNoticeHabit{
-							UserId:  userData.GetUserId(),
-							HabitId: h.Id,
-							EntryId: cron.EntryID(entryId),
-						})
-					}
+				for _, entryId := range entryIds {
+					store.CreateNewCronNoticeHabit(ctx, &cronnoticehabitmodel.CronNoticeHabit{
+						UserId:  userData.GetUserId(),
+						HabitId: h.Id,
+						EntryId: cron.EntryID(entryId),
+					})
 				}
-			}()
+			}
 
-			go func() {
-				listTask, err := taskStore.ListTaskByConditions(ctx, nil, nil, map[string]interface{}{"user_id": userData.GetUserId()})
+			listTask, err := taskStore.ListTaskByConditions(ctx, nil, nil, map[string]interface{}{"user_id": userData.GetUserId()})
+			if err != nil {
+				return err
+			}
+
+			for _, t := range listTask {
+				entryIds, err := appCtx.GetCronJob().CreateNewJobs(*common.NewNotificationBasedOnTask(
+					userData.GetUserId(),
+					t.Description,
+					t.Name,
+					t.Reminder),
+					sendNoticeFunc,
+					userData.GetLatestDvToken().DeviceToken,
+					fmt.Sprintf("Time to do %s", t.Name),
+					t.Name,
+				)
+
 				if err != nil {
-					return
+					return err
 				}
 
-				for _, t := range listTask {
-					entryIds, err := appCtx.GetCronJob().CreateNewJobs(*common.NewNotificationBasedOnTask(
-						userData.GetUserId(),
-						t.Description,
-						t.Name,
-						t.Reminder),
-						sendNoticeFunc,
-						userData.GetLatestDvToken().DeviceToken,
-						fmt.Sprintf("Time to do %s", t.Name),
-						t.Name,
-					)
-
-					if err != nil {
-						return
-					}
-
-					for _, entryId := range entryIds {
-						_store.CreateNewCronNoticeTask(ctx, &cronnoticetaskmodel.CronNoticeTask{
-							UserId:  userData.GetUserId(),
-							TaskId:  t.Id,
-							EntryId: cron.EntryID(entryId),
-						})
-					}
+				for _, entryId := range entryIds {
+					_store.CreateNewCronNoticeTask(ctx, &cronnoticetaskmodel.CronNoticeTask{
+						UserId:  userData.GetUserId(),
+						TaskId:  t.Id,
+						EntryId: cron.EntryID(entryId),
+					})
 				}
-			}()
+			}
 
 			return nil
 		},
